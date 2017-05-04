25489b5b877c66e1b55e839961407844c732ea7e
Nhan Nguyen <ntnhan0512@gmail.com>
2017-05-04 11:29:13 +0700

    Create sample-user login

--- app.js
+++ app.js
@@ -30,6 +30,30 @@ app.get('/greeting', (req, res) => {
   res.status(200).json({success: true, msg: 'Hello!'})
 })
 
+// Create example account
+app.get('/createFoo', (req, res) => {
+  let newUser = new User({username: 'foo', password: 'bar'})
+  newUser.save(err => {
+    if (err) return res.status(500).json({success: false})
+    res.status(200).json({success: true, user: newUser})
+  })
+})
+
+app.post('/login', (req, res) => {
+  // Find a user which match username and password
+  User.findOne({username: req.body.username, password: req.body.password}).exec((err, user) => {
+    if (err) return res.status(500).json({success: false})
+    if (!user) return res.status(404).json({success: false})
+
+    // create a token when login success,
+    // client will store this token in local and add to every request to restricted API
+    let token = jwt.sign({
+      _id: user._id
+    }, 'secret', {expiresIn: 60 * 60})
+    res.status(200).json({success: true, token})
+  })
+})
+
 server.listen(process.env.PORT || 8082, function () {
   console.log(`Listen on port ${process.env.PORT || '8082'}`)
 })
